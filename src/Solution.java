7815a5033f83d1714bf14f3df929c6a5e0b1025d
v sotnikov <sotnikov@rosplatforma.ru>
2023-12-25 14:50:27 +0300

    commit25122023

---
+++ src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+/*
+ThreadLocalRandom
+*/
+
+public class Solution {
+    public static int getRandomIntegerBetweenNumbers(int from, int to) {
+        return ThreadLocalRandom.current().nextInt(from,to);
+    }
+
+    public static double getRandomDouble() {
+        return ThreadLocalRandom.current().nextDouble(0,1);
+    }
+
+    public static long getRandomLongBetween0AndN(long n) {
+        return ThreadLocalRandom.current().nextLong(0,n);
+    }
+
+    public static void main(String[] args) {
+    }
+}
